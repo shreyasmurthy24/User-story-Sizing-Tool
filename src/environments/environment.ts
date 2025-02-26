ac8576c0dcbc289c16a869a566cdb04ab853e9e3
Shreyas Murthy 🐘 <ShreyasChitloorKrishna@amrock.com>
2025-02-26 20:32:23 +0000

    Merged PR 80712: New infra changes
    New infra changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@ export const environment = {
         clientId: `4WRplWBaxEr7c7782bA9ZePNr4WQ46GQ`,
         redirectUri: `${window.location.origin}/`,
         authorizationParams: {
-            audience: "urn:ql-api:sizingtool-backend-220024:Test",
+            audience: "urn:ql-api:tapsizingtool-backend-220024:Test",
             redirect_uri: window.location.origin
         },
         useRefreshTokens: true,
         appUri: "http://localhost:4200/",
     },
-    serverUrl: 'https://sizingtool-test.amrock-sb.foc.zone/',
+    serverUrl: 'https://tapsizingtool-test.amrock-sb.foc.zone/',
     //serverUrl: 'https://localhost:7259/',
     tokenServerUrl: 'https://sso.test.authrock.com/oauth/token'
 };
