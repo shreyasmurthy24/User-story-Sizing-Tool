e2a0b21e12386052a33b0773368b5f1267860c2a
ChitloorKrishna, ShreyasMurthy <ShreyasMurthyChitloorKrishna@rocketclose.com>
2025-04-20 20:27:37 +0000

    Merged PR 83593: Modified the app.get value
    Modified the app.get value

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const wss = new WebSocket.Server({ server });
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
